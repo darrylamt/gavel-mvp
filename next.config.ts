86cc7a3c3732a6c485e55808b6016b8a77200e7a
darrylamt <amoateydarryl4@gmail.com>
2026-03-03 07:14:34 +0000

    Remove webpack config, use Turbopack for Next.js 16

--- next.config.ts
+++ next.config.ts
@@ -1,13 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  webpack: (config, { isServer }) => {
-    config.watchOptions = {
-      ...config.watchOptions,
-      ignored: /apps\/mobile/,
-    };
-    return config;
-  },
+  turbopack: {},
 };
 
 export default nextConfig;
